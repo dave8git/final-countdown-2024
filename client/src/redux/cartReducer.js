8cceb648d38c079176f85d65c4f5188a428b9233
dave8git <dave8git@gmail.com>
2025-01-21 20:07:29 +0100

    Fix WIP - empty cart reducer

--- client/src/redux/cartReducer.js
+++ client/src/redux/cartReducer.js
@@ -9,36 +9,18 @@ const API_URL = 'http://localhost:8000/api';
 const reducerName = 'cart';
 const createActionName = name => `app/${reducerName}/${name}`;
 
-const START_REQUEST = createActionName('START_REQUEST');
-const END_REQUEST = createActionName('END_REQUEST');
-const ERROR_REQUEST = createActionName('ERROR_REQUEST');
-
-
-export const startRequest = payload => ({ payload, type: START_REQUEST });
-export const endRequest = payload => ({ payload, type: END_REQUEST });
-export const errorRequest = payload => ({ payload, type: ERROR_REQUEST });
-
-
 /* THUNKS */
-
-
 
 /* INITIAL STATE */
 const initialState = {
     data: [],
-    requests: {},
 };
 
 /* REDUCER */
 
 export default function reducer(statePart = initialState, action = {}) {
     switch (action.type) {
-        case START_REQUEST:
-            return { ...statePart, requests: { ...statePart.requests, [action.payload.name]: { pending: true, error: null, success: false } } };
-        case END_REQUEST:
-            return { ...statePart, requests: { ...statePart.requests, [action.payload.name]: { pending: true, error: null, success: true } } };
-        case ERROR_REQUEST:
-            return { ...statePart, requests: { ...statePart.requests, [action.payload.name]: { pending: false, error: action.payload.error, success: false } } };
+
         default:
             return statePart;
     }
